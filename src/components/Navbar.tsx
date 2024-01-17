b7aacb2340776dd11db5911147662d41eda67960
JivanKesan <jkesan28@gmail.com>
2024-01-16 22:29:54 -0500

    more updates

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,5 +1,3 @@
-import React from "react";
-
 const Navbar = () => {
   return (
     <nav className="bg-black dark:bg-gray-900 fixed w-full z-20 top-0 start-0 border-b border-gray-200 dark:border-gray-600">
