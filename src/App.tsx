d0ac9796173e0d4f50a8d136173dd64bf2829621
Aditya Kumar <adityak14200@gmail.com>
2024-08-11 01:22:50 -0400

    Route Changes

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,6 @@ import Home from "./components/Home";
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
 import Sponsor_page from "./components/Sponsor_page";
-import AboutUs from "./components/AboutUs";
 import About from "./components/AboutUs";
 import Sponsor from "./components/Sponsor";
 import Join from "./components/JoinUs";
@@ -17,7 +16,6 @@ const App: React.FC = () => {
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/sponsors" element={<Sponsor_page />} />
-          <Route path="/AboutUs" element={<AboutUs />} />
           <Route path="/About" element={<About />} />
           <Route path="/Sponsor" element={<Sponsor />} />
           <Route path="/Join" element={<Join />} />
